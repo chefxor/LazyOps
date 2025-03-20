d2e7f970ae00ce82cd4a18123e22f6cfbdf1e4f2
chef <saltnchefxor@proton.me>
2025-03-21 00:59:25 +0530

    add arch_setup.sh

---
+++ arch_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initial update & upgrade
+sudo pacman -Syyu
+
+# Installation of yay AUR handler
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# Installation of neccessary packages
+sudo pacman -S alacritty awesome rofi picom network-manager-applet ufw zsh neovim tmux kitty emacs fish stow xorg-xinit xclip flameshot tree lf nitrogen lxappearance pcmanfm ripgrep pass terminator rofi
+
+# Installing fonts
+yay -S ttf-hack-nerd ttf-firacode-nerd ttf-iosevkaterm-nerd ttf-mononoki-nerd ttf-terminus-nerd
